36f4561bda65078e93cc53c0196b5d2c24e7aa2e
c-builder <8412933+c-builder@users.noreply.github.com>
2023-08-31 01:35:32 +0800

    Create pageView.vue

---
+++ pageView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>评论功能示例</h1>
+
+    <deleteDialog />
+  </div>
+</template>
+
+<script setup>
+import { storeToRefs } from 'pinia';
+import Comment from '../components/comment.vue'; // 根据实际路径进行引入
+import useCommentStore from '../pinia/useCommentStore'; // 根据实际路径进行引入
+import deleteDialog from '../components/deleteDialog.vue';
+
+const store = useCommentStore();
+
+const {
+  comments,
+} = storeToRefs(store);
+</script>
