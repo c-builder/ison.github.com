56cb2c3823e884867e9e81ca2c3a22a0464a3e5e
c-builder <8412933+c-builder@users.noreply.github.com>
2023-09-01 00:34:34 +0800

    Create MessageComponent.vue

---
+++ MessageComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="message-container">
+    <transition-group name="message-fade" mode="out-in">
+      <div
+        class="message"
+        v-for="(message, index) in messageStore.messageQueue"
+        :key="index"
+      >
+        {{ message.text }}
+      </div>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { useMessageStore } from './messageStore';
+
+export default {
+  setup() {
+    const messageStore = useMessageStore();
+
+    return {
+      messageStore
+    };
+  }
+};
+</script>
+
+<style>
+.message-container {
+  position: relative;
+}
+.message-fade-enter-active,
+.message-fade-leave-active {
+  transition: all 0.5s;
+}
+.message-fade-enter,
+.message-fade-leave-to {
+  opacity: 0;
+}
+.message {
+  position: absolute;
+}
+</style>
