bb54de96d28b0550fc067ccc5fbe48d0a9553060
c-builder <8412933+c-builder@users.noreply.github.com>
2023-09-03 23:33:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 // main.js
 import { createApp } from 'vue';
 import App from './App.vue';
+import MyCustomComponent from './components/MyCustomComponent.vue';
 import MessagePlugin from './messagePlugin';
 
 const app = createApp(App);
 
 app.use(MessagePlugin);
+
+import videoComment from './components/videoComment.ce.vue';
+
+// 转换为自定义元素构造器
+const ExampleElement = defineCustomElement(videoComment);
+
+// 注册
+customElements.define('xs-vue-video', ExampleElement);
 
 app.mount('#app');
