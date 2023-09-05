824751d2db9655eea3bf0df11a1cd09146f8ee8d
c-builder <8412933+c-builder@users.noreply.github.com>
2023-09-06 00:30:54 +0800

    Update globalMixin.js

--- globalMixin.js
+++ globalMixin.js
@@ -3,21 +3,24 @@ import { useRoute } from 'vue-router';
 
 export default {
   mounted() {
-    const route = useRoute();
-    const currentPage = ref('');
-    // 获取当前页面的URL
-    currentPage.value = window.location.href;
-    // 获取路由的 meta 信息
-    const routeMeta = route.meta;
+    const init = () => {
+      const route = useRoute();
+      const currentPage = ref('');
+      // 获取当前页面的URL
+      currentPage.value = window.location.href;
+      // 获取路由的 meta 信息
+      const routeMeta = route.meta;
 
-    // 检查是否禁用混入
-    if (routeMeta.needCheck) {
-      const allPageLinks = document.querySelectorAll('a');
-      allPageLinks.forEach((pageLink) => {
-        pageLink.addEventListener('click', () => {
-          window.sessionStorage.setItem('previousPageUrl', currentPage.value);
+      // 检查是否禁用混入
+      if (routeMeta.needCheck) {
+        const allPageLinks = document.querySelectorAll('a');
+        allPageLinks.forEach((pageLink) => {
+          pageLink.addEventListener('click', () => {
+            window.sessionStorage.setItem('previousPageUrl', currentPage.value);
+          });
         });
-      });
-    }
+      }
+    };
+    requestIdleCallback(init);
   },
 };
