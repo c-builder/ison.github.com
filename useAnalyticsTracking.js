d48c1d59062fd66279b9915ec7bd29343105dfe9
c-builder <8412933+c-builder@users.noreply.github.com>
2023-09-06 23:21:08 +0800

    Update useAnalyticsTracking.js

--- useAnalyticsTracking.js
+++ useAnalyticsTracking.js
@@ -1,6 +1,6 @@
 // hooks/useAnalyticsTracking.js
 
-// from 全局来源名
+// from 全局来源名，如：首页，热榜，运营管理...
 export default function useAnalyticsTracking(from = '') {
   // linkUrl为将要重定向的地址
   function saveTrackPage(fromName, linkUrl) {
