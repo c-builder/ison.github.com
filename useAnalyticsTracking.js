bd0465c2e070e529cd4af63b1932652a5214323c
c-builder <8412933+c-builder@users.noreply.github.com>
2023-09-07 22:41:57 +0800

    Update useAnalyticsTracking.js

--- useAnalyticsTracking.js
+++ useAnalyticsTracking.js
@@ -5,8 +5,17 @@ export default function useAnalyticsTracking(from = '') {
   // linkUrl为将要重定向的地址
   function saveTrackPage(fromName, linkUrl) {
     // 通过linkUrl来判断是否符合跳转条件
-    // console.log(linkUrl); - console.log(document.referrer, '---');
-    if (fromName) {
+    
+    // 正则表达式匹配
+    const regex = /\/next\/#\/detail\?|\/next\/detail\/#\//;
+    
+    // 检查是否匹配任何一个部分或包含特定字符串
+    const isMatch = regex.test(linkUrl) || linkUrl.includes("/xsapi/user/v1/posts/redirect?uuid=");
+    
+    // 输出结果
+    console.log(`URL ${isMatch ? '包含' : '不包含'} '/next/#/detail?'、'/next/detail/#/' 或 '/xsapi/user/v1/posts/redirect?uuid='`);
+
+    if (isMatch && fromName) {
       window.localStorage.setItem('previousTrackPage', fromName);
     }
   }
