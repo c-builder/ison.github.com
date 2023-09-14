690cfb8bd952ef3f5a35d4448322d4c2ecdae8ce
c-builder <8412933+c-builder@users.noreply.github.com>
2023-09-15 01:23:52 +0800

    Create volume.vue

---
+++ volume.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Slider Example</h1>
+    <div class="wrap">
+      <Slider v-model="sliderValue" />
+    </div>
+    <p>Current Value: {{ sliderValue }}</p>
+  </div>
+</template>
+
+<script>
+import Slider from './Slider.vue';
+
+export default {
+  components: {
+    Slider,
+  },
+  data() {
+    return {
+      sliderValue: 30, // 初始值
+    };
+  },
+};
+</script>
+<style>
+.wrap {
+  width: 200px;
+}
+</style>
