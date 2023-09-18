ef3ff518d79bfe16320c7c023be302ee5084a032
c-builder <8412933+c-builder@users.noreply.github.com>
2023-09-18 23:50:38 +0800

    Create window.scrollY.vue

---
+++ window.scrollY.vue
@@ -0,0 +1,24 @@
+// 在组件中
+mounted() {
+  // 获取上次保存的滚动位置
+  const savedScrollPosition = localStorage.getItem('scrollPosition');
+  if (savedScrollPosition) {
+    window.scrollTo(0, parseInt(savedScrollPosition, 10));
+  }
+
+  // 监听滚动事件，保存滚动位置
+  window.addEventListener('scroll', this.saveScrollPosition);
+},
+
+beforeDestroy() {
+  // 在组件销毁前移除滚动事件监听
+  window.removeEventListener('scroll', this.saveScrollPosition);
+},
+
+methods: {
+  saveScrollPosition() {
+    // 保存当前滚动位置到localStorage
+    localStorage.setItem('scrollPosition', window.scrollY.toString());
+  }
+}
+window.scrollY
